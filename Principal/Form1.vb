8f85bac5f64b7de6c565d49d2e39604f838ce4bd
Márcio Fessini <fessini@gmail.com>
2021-01-14 20:14:12 -0300

    Comentário

--- Principal/Form1.vb
+++ Principal/Form1.vb
@@ -2,6 +2,7 @@
 
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Abre o form cliente para teste
         Dim frm As New frmCadCliente
         frm.Show()
     End Sub
